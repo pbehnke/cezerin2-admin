b8b56b09b44e9b285861764ade669bf3024cfb28
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-13 23:05:28 +0700

    Update

--- src/routes/apps/index.tsx
+++ src/routes/apps/index.tsx
@@ -1,20 +1,19 @@
 import React from "react"
-import { Switch, Route, NavLink } from "react-router-dom"
-import * as auth from "lib/webstoreAuth"
-import NotFound from "routes/notFound"
-import Login from "routes/apps/login"
-import Account from "modules/apps/account"
-import Services from "modules/apps/services"
-import ServiceDetails from "modules/apps/serviceDetails"
-import AppDetails from "modules/apps/appDetails"
+import { Route, Switch } from "react-router-dom"
+import Account from "../../modules/apps/account"
+import AppDetails from "../../modules/apps/appDetails"
+import ServiceDetails from "../../modules/apps/serviceDetails"
+import Services from "../../modules/apps/services"
+import Login from "../../routes/apps/login"
+import NotFound from "../../routes/notFound"
 
 export default () => (
-	<Switch>
-		<Route path="/apps" exact component={Services} />
-		<Route path="/apps/service/:serviceId" exect component={ServiceDetails} />
-		<Route path="/apps/app/:appKey" exect component={AppDetails} />
-		<Route path="/apps/login" exact component={Login} />
-		<Route path="/apps/account" exact component={Account} />
-		<Route component={NotFound} />
-	</Switch>
+  <Switch>
+    <Route path="/apps" exact component={Services} />
+    <Route path="/apps/service/:serviceId" exect component={ServiceDetails} />
+    <Route path="/apps/app/:appKey" exect component={AppDetails} />
+    <Route path="/apps/login" exact component={Login} />
+    <Route path="/apps/account" exact component={Account} />
+    <Route component={NotFound} />
+  </Switch>
 )
