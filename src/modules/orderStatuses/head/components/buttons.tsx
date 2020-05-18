5ebb7d9840b9de25e9f17c6122bb71bc1e493104
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-05-18 09:41:55 +0700

    Update buttons.tsx

--- src/modules/orderStatuses/head/components/buttons.tsx
+++ src/modules/orderStatuses/head/components/buttons.tsx
@@ -1,73 +1,67 @@
 import FontIcon from "material-ui/FontIcon"
 import IconButton from "material-ui/IconButton"
-import React from "react"
+import React, { useState } from "react"
 import messages from "../../../../lib/text"
 import DeleteConfirmation from "../../../../modules/shared/deleteConfirmation"
 
-class Buttons extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      openDelete: false,
-    }
+const Buttons = props => {
+  const [openDelete, setOpenDelete] = useState(false)
+
+  const showDelete = () => {
+    setOpenDelete(true)
   }
 
-  showDelete = () => {
-    this.setState({ openDelete: true })
+  const closeDelete = () => {
+    setOpenDelete(false)
   }
 
-  closeDelete = () => {
-    this.setState({ openDelete: false })
+  const deleteStatus = () => {
+    setOpenDelete(false)
+    props.onDelete(props.selected.id)
   }
 
-  deleteStatus = () => {
-    this.setState({ openDelete: false })
-    this.props.onDelete(this.props.selected.id)
-  }
+  const { selected, onCreate } = props
+  const statusName =
+    selected && selected.name && selected.name.length > 0
+      ? selected.name
+      : "Draft"
 
-  render() {
-    const { selected, onDelete, onCreate } = this.props
-    const statusName =
-      selected && selected.name && selected.name.length > 0
-        ? selected.name
-        : "Draft"
-
-    return (
-      <span>
-        {selected && (
-          <>
-            <IconButton
-              touch
-              tooltip={messages.actions_delete}
-              tooltipPosition="bottom-left"
-              onClick={this.showDelete}
-            >
-              <FontIcon color="#fff" className="material-icons">
-                delete
-              </FontIcon>
-            </IconButton>
-            <DeleteConfirmation
-              open={this.state.openDelete}
-              isSingle
-              itemsCount={1}
-              itemName={statusName}
-              onCancel={this.closeDelete}
-              onDelete={this.deleteStatus}
-            />
-          </>
-        )}
-        <IconButton
-          touch
-          tooltipPosition="bottom-left"
-          tooltip={messages.addOrderStatus}
-          onClick={onCreate}
-        >
-          <FontIcon color="#fff" className="material-icons">
-            add
-          </FontIcon>
-        </IconButton>
-      </span>
-    )
-  }
+  return (
+    <span>
+      {selected && (
+        <>
+          <IconButton
+            touch
+            tooltip={messages.actions_delete}
+            tooltipPosition="bottom-left"
+            onClick={showDelete}
+          >
+            <FontIcon color="#fff" className="material-icons">
+              delete
+            </FontIcon>
+          </IconButton>
+          <DeleteConfirmation
+            open={openDelete}
+            isSingle
+            itemsCount={1}
+            itemName={statusName}
+            onCancel={closeDelete}
+            onDelete={deleteStatus}
+          />
+        </>
+      )}
+      <IconButton
+        touch
+        tooltipPosition="bottom-left"
+        tooltip={messages.addOrderStatus}
+        onClick={onCreate}
+      >
+        <FontIcon color="#fff" className="material-icons">
+          add
+        </FontIcon>
+      </IconButton>
+    </span>
+  )
 }
+
 export default Buttons
