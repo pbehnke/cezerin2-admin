77283ce26d0489f1fe374f05ecf8066cb96573dd
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-04 05:48:30 +0700

    Update summary.tsx

--- src/modules/customers/edit/components/summary.tsx
+++ src/modules/customers/edit/components/summary.tsx
@@ -1,108 +1,102 @@
 import Dialog from "@material-ui/core/Dialog"
 import Paper from "@material-ui/core/Paper"
 import RaisedButton from "material-ui/RaisedButton"
-import React from "react"
+import React, { useState } from "react"
 import * as helper from "../../../../lib/helper"
 import messages from "../../../../lib/text"
 import style from "./style.css"
 import SummaryForm from "./summaryForm.js"
 
-class CustomerSummary extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      openSummaryEdit: false,
-    }
+const CustomerSummary = props => {
+  const [openSummaryEdit, setOpenSummaryEdit] = useState(false)
+
+  const showSummaryEdit = () => {
+    setOpenSummaryEdit(true)
   }
 
-  showSummaryEdit = () => {
-    this.setState({ openSummaryEdit: true })
+  const hideSummaryEdit = () => {
+    setOpenSummaryEdit(false)
   }
 
-  hideSummaryEdit = () => {
-    this.setState({ openSummaryEdit: false })
+  const saveSummaryEdit = customer => {
+    props.onCustomerSummaryUpdate(customer)
+    hideSummaryEdit()
   }
 
-  saveSummaryEdit = customer => {
-    this.props.onCustomerSummaryUpdate(customer)
-    this.hideSummaryEdit()
-  }
+  const { customer, settings } = props
+  const totalSpent = helper.formatCurrency(customer.total_spent, settings)
 
-  render() {
-    const { customer, settings } = this.props
-    const totalSpent = helper.formatCurrency(customer.total_spent, settings)
+  return (
+    <Paper className="paper-box" zDepth={1}>
+      <div className={style.innerBox}>
+        <div
+          className={style.customerName}
+          style={{ paddingBottom: 26, paddingTop: 0 }}
+        >
+          {customer.full_name}
+          <>
+            <small>{customer.group_name}</small>
+          </>
+        </div>
 
-    return (
-      <Paper className="paper-box" zDepth={1}>
-        <div className={style.innerBox}>
-          <div
-            className={style.customerName}
-            style={{ paddingBottom: 26, paddingTop: 0 }}
-          >
-            {customer.full_name}
-            <>
-              <small>{customer.group_name}</small>
-            </>
+        <div className={`${style.summaryRow} row`}>
+          <div className="col-xs-5">
+            <span>{messages.email}</span>
           </div>
-
-          <div className={`${style.summaryRow} row`}>
-            <div className="col-xs-5">
-              <span>{messages.email}</span>
-            </div>
-            <div className="col-xs-7">
-              <a href={`MailTo:${customer.email}`} className={style.link}>
-                {customer.email}
-              </a>
-            </div>
+          <div className="col-xs-7">
+            <a href={`MailTo:${customer.email}`} className={style.link}>
+              {customer.email}
+            </a>
           </div>
+        </div>
 
-          <div className={`${style.summaryRow} row`}>
-            <div className="col-xs-5">
-              <span>{messages.mobile}</span>
-            </div>
-            <div className="col-xs-7">{customer.mobile}</div>
+        <div className={`${style.summaryRow} row`}>
+          <div className="col-xs-5">
+            <span>{messages.mobile}</span>
           </div>
+          <div className="col-xs-7">{customer.mobile}</div>
+        </div>
 
-          <div className={`${style.summaryRow} row`}>
-            <div className="col-xs-5">
-              <span>{messages.customers_totalSpent}</span>
-            </div>
-            <div className="col-xs-7">{totalSpent}</div>
+        <div className={`${style.summaryRow} row`}>
+          <div className="col-xs-5">
+            <span>{messages.customers_totalSpent}</span>
           </div>
+          <div className="col-xs-7">{totalSpent}</div>
+        </div>
 
-          <div className={`${style.summaryRow} row`}>
-            <div className="col-xs-5">
-              <span>{messages.note}</span>
-            </div>
-            <div className="col-xs-7">{customer.note}</div>
+        <div className={`${style.summaryRow} row`}>
+          <div className="col-xs-5">
+            <span>{messages.note}</span>
           </div>
+          <div className="col-xs-7">{customer.note}</div>
+        </div>
 
-          <div style={{ marginTop: 20 }}>
-            <RaisedButton
-              label="Edit"
-              style={{ marginRight: 15 }}
-              onClick={this.showSummaryEdit}
+        <div style={{ marginTop: 20 }}>
+          <RaisedButton
+            label="Edit"
+            style={{ marginRight: 15 }}
+            onClick={showSummaryEdit}
+          />
+        </div>
+
+        <Dialog
+          title={messages.customers_titleEdit}
+          modal={false}
+          open={openSummaryEdit}
+          onRequestClose={hideSummaryEdit}
+          contentStyle={{ width: 600 }}
+        >
+          <div style={{ width: "500px", margin: "25px" }}>
+            <SummaryForm
+              initialValues={customer}
+              onCancel={hideSummaryEdit}
+              onSubmit={saveSummaryEdit}
             />
           </div>
-
-          <Dialog
-            title={messages.customers_titleEdit}
-            modal={false}
-            open={this.state.openSummaryEdit}
-            onRequestClose={this.hideSummaryEdit}
-            contentStyle={{ width: 600 }}
-          >
-            <div style={{ width: "500px", margin: "25px" }}>
-              <SummaryForm
-                initialValues={customer}
-                onCancel={this.hideSummaryEdit}
-                onSubmit={this.saveSummaryEdit}
-              />
-            </div>
-          </Dialog>
-        </div>
-      </Paper>
-    )
-  }
+        </Dialog>
+      </div>
+    </Paper>
+  )
 }
+
 export default CustomerSummary
