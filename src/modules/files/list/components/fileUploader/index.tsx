0516627802cb3a597dde4ffb4988f1a700fb45e5
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-04 05:50:24 +0700

    As module

--- src/modules/files/list/components/fileUploader/index.tsx
+++ src/modules/files/list/components/fileUploader/index.tsx
@@ -3,7 +3,7 @@ import Snackbar from "material-ui/Snackbar"
 import React from "react"
 import Dropzone from "react-dropzone"
 import messages from "../../../../../lib/text"
-import style from "./style.css"
+import style from "./style.module.css"
 
 class MultiUploader extends React.Component {
   onDrop = files => {
