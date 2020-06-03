89eda8bbe83761287ea8dc00140db5fd6833a3c1
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-04 05:33:18 +0700

    Update form.tsx

--- src/modules/settings/generalLogo/components/form.tsx
+++ src/modules/settings/generalLogo/components/form.tsx
@@ -1,29 +1,27 @@
 import Paper from "@material-ui/core/Paper"
-import React from "react"
+import React, { useEffect } from "react"
 import ImageUpload from "../../../../modules/shared/imageUpload"
 
-class GeneralLogoSettingsForm extends React.Component {
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const GeneralLogoSettingsForm = props => {
+  useEffect(() => {
+    props.onLoad()
+  }, [])
 
-  render() {
-    const { onImageUpload, onImageDelete, settings } = this.props
-    const imageUrl = settings && settings.logo ? settings.logo : ""
+  const { onImageUpload, onImageDelete, settings } = props
+  const imageUrl = settings && settings.logo ? settings.logo : ""
 
-    return (
-      <Paper className="paper-box" zDepth={1}>
-        <div style={{ padding: 30 }}>
-          <ImageUpload
-            uploading={false}
-            imageUrl={imageUrl}
-            onDelete={onImageDelete}
-            onUpload={onImageUpload}
-          />
-        </div>
-      </Paper>
-    )
-  }
+  return (
+    <Paper className="paper-box" zDepth={1}>
+      <div style={{ padding: 30 }}>
+        <ImageUpload
+          uploading={false}
+          imageUrl={imageUrl}
+          onDelete={onImageDelete}
+          onUpload={onImageUpload}
+        />
+      </div>
+    </Paper>
+  )
 }
 
 export default GeneralLogoSettingsForm
