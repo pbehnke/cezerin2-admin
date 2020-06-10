cd0ad563d1b056177e028085fd3f24beca7842a0
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-10 07:58:14 +0700

    Removing css

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from "react"
 import { render } from "@testing-library/react"
-import App from "./App"
+import React from "react"
+import App from "./app"
 
-test("renders learn react link", () => {
+test("renders dashboard", () => {
   const { getByText } = render(<App />)
-  const linkElement = getByText(/learn react/i)
+  const linkElement = getByText(/Dashboard/i)
   expect(linkElement).toBeInTheDocument()
 })
