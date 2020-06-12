af11538d8ef3a85761e47b313cf30ec589aa259f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-13 06:55:46 +0700

    Update edit.tsx

--- src/routes/pages/edit.tsx
+++ src/routes/pages/edit.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import PageEdit from "../../modules/pages/edit"
 
-const ProductDetails = props => (
+const ProductDetails = (props: Readonly<{}>) => (
   <div className="row row--no-gutter col-full-height scroll">
     <div className="col-xs-12 col-sm-12 col-md-10 col-lg-8 col-md-offset-1 col-lg-offset-2">
       <PageEdit {...props} />
