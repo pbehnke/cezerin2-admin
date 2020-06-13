18f44100f3de24aa13f4fdad285c833184c31e84
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-06-13 10:05:32 +0700

    Update Editor

--- src/modules/shared/editor/index.tsx
+++ src/modules/shared/editor/index.tsx
@@ -1,27 +1,26 @@
-import Editor from "editorjs"
-import React from "react"
+import EditorJS from "@editorjs/editorjs"
 
-const config = {
-  inline: true,
-  plugins: [
-    "autolink lists link image charmap preview anchor",
-    "searchreplace visualblocks code fullscreen",
-    "media table paste code textcolor directionality",
-  ],
-  toolbar1:
-    "image media | styleselect | bold italic bullist numlist link alignleft aligncenter alignright alignjustify",
-  toolbar2:
-    "undo redo | forecolor paste removeformat table | outdent indent | preview code",
-}
+// const config = {
+//   inline: true,
+//   plugins: [
+//     "autolink lists link image charmap preview anchor",
+//     "searchreplace visualblocks code fullscreen",
+//     "media table paste code textcolor directionality",
+//   ],
+//   toolbar1:
+//     "image media | styleselect | bold italic bullist numlist link alignleft aligncenter alignright alignjustify",
+//   toolbar2:
+//     "undo redo | forecolor paste removeformat table | outdent indent | preview code",
+// }
 
-const Editor = props => {
-  const onChange = e => {
-    const content = e.target.getContent()
-    //setState({ value: content })
-    props.input.onChange(content)
-  }
+const Editor = new EditorJS()
+// const onChange = e => {
+//   const content = e.target.getContent()
+//   //setState({ value: content })
+//   props.input.onChange(content)
+// }
 
-  return <Editor onChange={onChange} />
-}
+//   return <EditorJS />
+// }
 
 export default Editor
